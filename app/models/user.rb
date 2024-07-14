0666b9138510846e9b4cc94fe22be61f38a12691
ikari1055 <takanori.ikari@gmail.com>
2024-07-14 14:45:29 +0900

    不要なバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
 
   # バリデーションの設定
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
-  validates :password_confirmation, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
