299a7d34aa884234e731df79d0921fee4950f9cd
ikari1055 <takanori.ikari@gmail.com>
2024-08-04 17:09:03 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show, :destroy]
+  # before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :authenticate_user!, only: [:new, :create] # ログインしているユーザーのみアクセス許可
 
   def index
