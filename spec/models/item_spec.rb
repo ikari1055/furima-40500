cdce19f10fbd43f295d6bc9a487eecb229716152
ikari1055 <takanori.ikari@gmail.com>
2024-08-04 15:32:01 +0900

    ---の追記とID値の代入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -26,31 +26,31 @@ RSpec.describe Item, type: :model do
     end
 
     it 'カテゴリーが選択されていないと登録できない' do
-      @item.category_id = nil
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
 
     it '商品の状態が選択されていないと登録できない' do
-      @item.condition_id = nil
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Condition を選択してください')
     end
 
     it '配送料の負担が選択されていないと登録できない' do
-      @item.shipping_cost_id = nil
+      @item.shipping_cost_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
     end
 
     it '発送元の地域が選択されていないと登録できない' do
-      @item.prefecture_id = nil
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
 
     it '発送までの日数が選択されていないと登録できない' do
-      @item.shipping_days_id = nil
+      @item.shipping_days_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping days can't be blank")
     end
