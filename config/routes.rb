0cc86d3f5d43597118247084d5dd475815c226aa
ikari1055 <takanori.ikari@gmail.com>
2024-08-07 21:04:41 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/show'
   devise_for :users
-  resources :items, only: [:new, :create, :index, :show]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :categories, only: [:show]  # ここでcategoriesリソースを追加
 
   root 'items#index'
 end
