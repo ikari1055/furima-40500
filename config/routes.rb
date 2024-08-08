71ad5624e517b6b979e39fef23065e780279b519
ikari1055 <takanori.ikari@gmail.com>
2024-08-08 10:43:41 +0900

    コントローラー削除とルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/show'
   devise_for :users
   resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :categories, only: [:show]  # ここでcategoriesリソースを追加
 
   root 'items#index'
 end
