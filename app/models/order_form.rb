e2aebb1f6c5c66c0980226b181580162eac1d536
ikari1055 <takanori.ikari@gmail.com>
2024-08-12 20:27:27 +0900

    with_optionsの使用

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,15 +3,16 @@ class OrderForm
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
   # バリデーションの定義
-  validates :token, presence: { message: "can't be blank" }
-  validates :postal_code, presence: { message: "can't be blank" },
-                          format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  with_options presence: { message: "can't be blank" } do
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' },
+                             length: { minimum: 10, message: 'is too short' }
+  end
+
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, presence: { message: "can't be blank" }
-  validates :addresses, presence: { message: "can't be blank" }
-  validates :phone_number, presence: { message: "can't be blank" },
-                           format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' },
-                           length: { minimum: 10, message: 'is too short' }
 
   # カスタムバリデーションでuser_idとitem_idのエラーを削除
   validate :exclude_user_and_item_error_messages
