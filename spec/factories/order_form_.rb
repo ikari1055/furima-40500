604dd6ce75824c75656d648586de89729255b22f
ikari1055 <takanori.ikari@gmail.com>
2024-08-13 15:24:50 +0900

    記述不要

--- spec/factories/order_form_.rb
+++ spec/factories/order_form_.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     building         { '東京ハイツ' }
     phone_number     { '09012345678' }
     token            { 'tok_abcdefghijk00000000000000000' } # 仮のトークンデータ
-    user_id { create(:user).id }
-    item_id { create(:item).id }
   end
 end
